18eb0c4d82baf283f264024e741c02e57848914e
unknown <qx690@nyu.edu>
2024-12-10 23:22:56 -0500

    Updates

--- evaluator.py
+++ evaluator.py
@@ -113,9 +113,12 @@ class ProbingEvaluator:
             for batch in tqdm(dataset, desc="Probe prediction step"):
                 ################################################################################
                 # TODO: Forward pass through your model
-                init_states = batch.states[:, 0:1].to(self.device)
+                states = batch.states.to(self.device)  # 不仅取0:1，而是全部时间步
                 actions = batch.actions.to(self.device)
-                # init_states = batch.states[:, 0:1]  # BS, 1, C, H, W
+                with torch.no_grad():
+                    pred_encs = model.online_encoder(states)  # (B,T,D) T与target保持一致
+                    pred_encs = pred_encs.transpose(0, 1)
+                    # init_states = batch.states[:, 0:1]  # BS, 1, C, H, W
                 # pred_encs = model(states=init_states, actions=batch.actions)
                 # pred_encs = pred_encs.transpose(0, 1)  # # BS, T, D --> T, BS, D
 
@@ -145,10 +148,6 @@ class ProbingEvaluator:
                 ################################################################################
 
                 # pred_encs = pred_encs.detach()
-
-                with torch.no_grad():
-                    pred_encs = model.online_encoder(init_states)
-                    pred_encs = pred_encs.transpose(0, 1)
 
                 n_steps = pred_encs.shape[0]
                 bs = pred_encs.shape[1]
@@ -242,9 +241,9 @@ class ProbingEvaluator:
         for idx, batch in enumerate(tqdm(val_ds, desc="Eval probe pred")):
             ################################################################################
             # TODO: Forward pass through your model
-            init_states = batch.states[:, 0:1].to(self.device)  # BS, 1, C, H, W
+            states = batch.states.to(self.device)
             actions = batch.actions.to(self.device)
-            pred_encs = model.online_encoder(init_states)
+            pred_encs = model.online_encoder(states)  # (B,T,D)
             pred_encs = pred_encs.transpose(0, 1)
             # init_states = batch.states[:, 0:1]  # BS, 1 C, H, W
             # pred_encs = model(states=init_states, actions=batch.actions)
