cc4e0e54a3bec750a91f66b85043daacedd4e011
unknown <qx690@nyu.edu>
2024-12-10 23:04:05 -0500

    Updates

--- evaluator.py
+++ evaluator.py
@@ -70,7 +70,8 @@ class ProbingEvaluator:
         """
         Probes whether the predicted embeddings capture the future locations
         """
-        repr_dim = self.model.repr_dim
+        # repr_dim = self.model.repr_dim
+        repr_dim = 128
         dataset = self.ds
         model = self.model
 
@@ -112,9 +113,11 @@ class ProbingEvaluator:
             for batch in tqdm(dataset, desc="Probe prediction step"):
                 ################################################################################
                 # TODO: Forward pass through your model
-                init_states = batch.states[:, 0:1]  # BS, 1, C, H, W
-                pred_encs = model(states=init_states, actions=batch.actions)
-                pred_encs = pred_encs.transpose(0, 1)  # # BS, T, D --> T, BS, D
+                init_states = batch.states[:, 0:1].to(self.device)
+                actions = batch.actions.to(self.device)
+                # init_states = batch.states[:, 0:1]  # BS, 1, C, H, W
+                # pred_encs = model(states=init_states, actions=batch.actions)
+                # pred_encs = pred_encs.transpose(0, 1)  # # BS, T, D --> T, BS, D
 
                 # #2. recurrent - unrolling embeddings (uncomment this section and comment out other section as needed)
                 # pred_encs = []
@@ -141,7 +144,11 @@ class ProbingEvaluator:
                 # Make sure pred_encs has shape (T, BS, D) at this point
                 ################################################################################
 
-                pred_encs = pred_encs.detach()
+                # pred_encs = pred_encs.detach()
+
+                with torch.no_grad():
+                    pred_encs = model.online_encoder(init_states)
+                    pred_encs = pred_encs.transpose(0, 1)
 
                 n_steps = pred_encs.shape[0]
                 bs = pred_encs.shape[1]
@@ -232,10 +239,14 @@ class ProbingEvaluator:
         for idx, batch in enumerate(tqdm(val_ds, desc="Eval probe pred")):
             ################################################################################
             # TODO: Forward pass through your model
-            init_states = batch.states[:, 0:1]  # BS, 1 C, H, W
-            pred_encs = model(states=init_states, actions=batch.actions)
-            # # BS, T, D --> T, BS, D
+            init_states = batch.states[:, 0:1].to(self.device)  # BS, 1, C, H, W
+            actions = batch.actions.to(self.device)
+            pred_encs = model.online_encoder(init_states)
             pred_encs = pred_encs.transpose(0, 1)
+            # init_states = batch.states[:, 0:1]  # BS, 1 C, H, W
+            # pred_encs = model(states=init_states, actions=batch.actions)
+            # # # BS, T, D --> T, BS, D
+            # pred_encs = pred_encs.transpose(0, 1)
 
             # #2. recurrent - unrolling embeddings (uncomment this section and comment out other section as needed)
             # pred_encs = []
